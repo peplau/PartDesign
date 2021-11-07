511d8fc1e6f92e9b09c16f754e0650ad4c0e5211
Rodrigo Peplau <rodrigo.peplau@altudo.co>
2021-11-07 14:05:21 -0300

    Had to temporarly remove the Custom Experience Button

--- src/Feature/PartDesignMvc/code/Commands/EditPartDesign.cs
+++ src/Feature/PartDesignMvc/code/Commands/EditPartDesign.cs
@@ -2,6 +2,7 @@
 using System.Web;
 using Sitecore.Configuration;
 using Sitecore.Diagnostics;
+using Sitecore.Links;
 using Sitecore.Shell.Applications.WebEdit.Commands;
 using Sitecore.Shell.Framework.Commands;
 using Sitecore.Web.UI.Sheer;
@@ -14,23 +15,24 @@ namespace Feature.PartDesign.Mvc.Commands
     {
       // Validations
       Assert.ArgumentNotNull(context, nameof(context));
-      if (context.Items.Length != 1)
-        return;
 
       var datasourceItem = context.Items.FirstOrDefault();
+      if (datasourceItem == null)
+        return;
+
+      var targetUrl = LinkManager.GetItemUrl(datasourceItem);
 
       // Window URL
-      var windowUrl = $"/?sc_mode=edit" +
-        $"&sc_itemid={HttpUtility.UrlEncode(datasourceItem.ID.ToString())}" +
-        $"&sc_site={Sitecore.Context.Site.Name}";
-      var fullScript = "top.location.href='" + windowUrl + "';";
+      //var windowUrl = $"/?sc_mode=edit&sc_itemid={HttpUtility.UrlEncode(datasourceItem.ID.ToString())}&sc_site={Sitecore.Context.Site.Name}";
+      //var fullScript = "top.location.href='" + windowUrl + "';";
+      var fullScript = "top.location.href='" + targetUrl + "';";
 
       // Confirm is only necessary if the item was modified
-      if (SheerResponse.CheckModified())
-      {
+      //if (SheerResponse.CheckModified())
+      //{
         var confirmMessage = Settings.GetSetting("PartDesign.EditConfirmMessage", Constants.EditPartDesignConfirmMessage);
         fullScript = "if (confirm('" + confirmMessage + "')) { " + fullScript + " }";
-      }
+      //}
 
       // Triggering the window
       SheerResponse.Eval(fullScript);
